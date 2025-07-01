ad8d3af9b3b0a008407086a47c0d885561d1af0e
Tomas Rigaux <trigaux@cisco.com>
2025-07-01 15:26:11 -0400

    Improve module API.

--- include/flow/core/Module.hpp
+++ include/flow/core/Module.hpp
@@ -5,9 +5,10 @@
 
 #include "Core.hpp"
 
-#include <nlohmann/json.hpp>
+#include <nlohmann/json_fwd.hpp>
 
 #include <filesystem>
+#include <optional>
 #include <string>
 #include <vector>
 
@@ -17,15 +18,23 @@ using json = nlohmann::json;
 
 class NodeFactory;
 
+/**
+ * @brief Structure to hold metadata for a flow module.
+ */
 struct ModuleMetaData
 {
+    /// The name of the module.
     std::string Name;
+
+    /// The version of the module in semantic versioning format (e.g., "1.0.0").
     std::string Version;
+
+    /// The author of the module.
     std::string Author;
+
+    /// A description of the module.
     std::string Description;
 };
-
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(ModuleMetaData, Name, Version, Author, Description);
 
 class Module
 {
@@ -36,40 +45,71 @@ class Module
 
   public:
     /**
-     * @brief Constructs a flow module from a given directory.
+     * @brief Constructs a flow module with a given NodeFactory.
+     * @param factory The factory to load registered nodes into.
+     */
+    Module(std::shared_ptr<NodeFactory> factory);
+
+    /**
+     * @brief Constructs a flow module from a given JSON metadata and directory.
      * @param dir The directory to try to load.
      * @param factory The factory to load registered nodes into.
      */
     Module(const std::filesystem::path& dir, std::shared_ptr<NodeFactory> factory);
 
-    Module(const json& module_json, const std::filesystem::path& dir, std::shared_ptr<NodeFactory> factory);
-
+    /**
+     * @brief Destructor for the Module class. Unloads the module if loaded.
+     */
     ~Module();
 
     /**
      * @brief Loads a module from a given directory.
      * @param dir The directory to try to load.
+     * @return True if the module was loaded successfully, false otherwise.
      */
     bool Load(const std::filesystem::path& dir);
 
     /**
      * @brief Unloads the currently loaded module handle.
+     * @return True if the module was unloaded successfully, false otherwise.
      */
     bool Unload();
 
+    /**
+     * @brief Registers the module nodes with the provided factory.
+     * @param factory The factory to register nodes with.
+     */
+    void RegisterModuleNodes(const std::shared_ptr<NodeFactory>& factory);
+
+    /**
+     * @brief Unregisters the module nodes from the provided factory.
+     * @param factory The factory to unregister nodes from.
+     */
+    void UnregisterModuleNodes(const std::shared_ptr<NodeFactory>& factory);
+
+    /**
+     * @brief Checks if the module is currently loaded.
+     * @return True if loaded, false otherwise.
+     */
     bool IsLoaded() const noexcept { return _handle != nullptr; }
 
-    const ModuleMetaData& GetMetaData() const noexcept { return _metadata; }
+    /**
+     * @brief Gets the metadata associated with the module.
+     * @return A const reference to the ModuleMetaData.
+     */
+    const std::optional<ModuleMetaData>& GetMetaData() const noexcept { return _metadata; }
 
   private:
     void ValidateMetaData(const json& module_json);
 
   public:
+    /**
+     * @brief The file extension for module metadata files.
+     */
     static const std::string FileExtension;
-    static const std::string BinaryExtension;
 
   private:
-    ModuleMetaData _metadata;
+    std::optional<ModuleMetaData> _metadata;
     std::vector<std::string> _dependencies;
 
     std::shared_ptr<NodeFactory> _factory;
--- tests/module_test.cpp
+++ tests/module_test.cpp
@@ -17,7 +17,34 @@ const std::filesystem::path module_path = std::filesystem::current_path() / "tes
 auto factory = std::make_shared<NodeFactory>();
 auto env     = Env::Create(factory);
 
-TEST(ModuleTest, Load) { ASSERT_NO_THROW(Module(module_path, factory)); }
+TEST(ModuleTest, Load)
+{
+    Module m(factory);
+    ASSERT_NO_THROW(ASSERT_TRUE(m.Load(module_path)));
+    ASSERT_TRUE(m.IsLoaded());
+}
+
+TEST(ModuleTest, Unload)
+{
+    Module module(module_path, factory);
+    ASSERT_TRUE(module.IsLoaded());
+    ASSERT_TRUE(module.Unload());
+    ASSERT_FALSE(module.IsLoaded());
+}
+
+TEST(ModuleTest, LoadInvalidPath)
+{
+    Module m(factory);
+    ASSERT_THROW(m.Load("invalid_path.fmod"), std::runtime_error);
+}
+
+TEST(ModuleTest, UnloadWithoutLoad)
+{
+    Module m(factory);
+    ASSERT_FALSE(m.IsLoaded());
+    ASSERT_FALSE(m.Unload());
+    ASSERT_FALSE(m.IsLoaded());
+}
 
 TEST(ModuleTest, ValidateMetaData)
 {
@@ -25,10 +52,31 @@ TEST(ModuleTest, ValidateMetaData)
     ASSERT_TRUE(module.IsLoaded());
 
     const auto& meta_data = module.GetMetaData();
-    ASSERT_EQ(meta_data.Name, "test_module");
-    ASSERT_EQ(meta_data.Version, "0.0.0");
-    ASSERT_EQ(meta_data.Author, "Cisco Systems, Inc.");
-    ASSERT_EQ(meta_data.Description, "A test module.");
+    ASSERT_EQ(meta_data->Name, "test_module");
+    ASSERT_EQ(meta_data->Version, "0.0.0");
+    ASSERT_EQ(meta_data->Author, "Cisco Systems, Inc.");
+    ASSERT_EQ(meta_data->Description, "A test module.");
+}
+
+TEST(ModuleTest, RegisterModuleNodes)
+{
+    Module module(module_path, factory);
+    ASSERT_TRUE(module.IsLoaded());
+
+    ASSERT_NO_THROW(module.RegisterModuleNodes(factory));
+    ASSERT_NE(factory->CreateNode("TestNode", UUID{}, "test", env), nullptr);
+}
+
+TEST(ModuleTest, UnregisterModuleNodes)
+{
+    Module module(module_path, factory);
+    ASSERT_TRUE(module.IsLoaded());
+
+    module.RegisterModuleNodes(factory);
+    ASSERT_NE(factory->CreateNode("TestNode", UUID{}, "test", env), nullptr);
+
+    ASSERT_NO_THROW(module.UnregisterModuleNodes(factory));
+    ASSERT_EQ(factory->CreateNode("TestNode", UUID{}, "test", env), nullptr);
 }
 
 TEST(ModuleTest, RunModuleNodes)
@@ -36,30 +84,12 @@ TEST(ModuleTest, RunModuleNodes)
     Module module(module_path, factory);
     ASSERT_TRUE(module.IsLoaded());
 
+    module.RegisterModuleNodes(factory);
+
     SharedNode node;
     ASSERT_NO_THROW(node = factory->CreateNode("TestNode", UUID{}, "test", env));
     ASSERT_NE(node, nullptr);
     node->OnCompute.Bind("Test", [&] { FAIL(); });
     node->OnError.Bind("Test", [&](auto&& e) { ASSERT_THROW(throw e, std::exception); });
     node->InvokeCompute();
-}
-
-TEST(ModuleTest, Unload)
-{
-    Module module(module_path, factory);
-    ASSERT_TRUE(module.IsLoaded());
-    ASSERT_NO_THROW(module.Unload());
-    ASSERT_FALSE(module.IsLoaded());
-}
-
-TEST(ModuleTest, LoadUnloadLoad)
-{
-    Module module(module_path, factory);
-    ASSERT_TRUE(module.IsLoaded());
-    ASSERT_NO_THROW(ASSERT_FALSE(module.Load(module_path)));
-    ASSERT_TRUE(module.IsLoaded());
-    ASSERT_NO_THROW(ASSERT_TRUE(module.Unload()));
-    ASSERT_FALSE(module.IsLoaded());
-    ASSERT_NO_THROW(ASSERT_TRUE(module.Load(module_path)));
-    ASSERT_TRUE(module.IsLoaded());
 }
